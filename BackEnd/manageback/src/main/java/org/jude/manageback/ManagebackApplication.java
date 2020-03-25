79c77132adc1b69aefaf1b18551a3a76f937f9ec
judywink <1040984760@qq.com>
2020-03-25 11:04:21 +0800

    使用Spring Initializr快速搭建Spring Boot项目

---
+++ BackEnd/manageback/src/main/java/org/jude/manageback/ManagebackApplication.java
@@ -0,0 +1,13 @@
+package org.jude.manageback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagebackApplication.class, args);
+    }
+
+}
