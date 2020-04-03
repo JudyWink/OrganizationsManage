62ec1f9f8dca0410c71f474fe0733666bbffa883
judywink <1040984760@qq.com>
2020-04-03 17:05:39 +0800

    成功创建Controller并启动

--- BackEnd/managefront/src/main/java/org/jude/managefront/ManagefrontApplication.java
+++ BackEnd/managefront/src/main/java/org/jude/managefront/ManagefrontApplication.java
@@ -1,13 +1,13 @@
 package org.jude.managefront;
 
-//Spring Boot的启动类 一定要加@SpringBootApplication
+//Spring Boot的启动类
+// 一定要加@SpringBootApplication
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ManagefrontApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ManagefrontApplication.class, args);
     }
---
+++ BackEnd/managefront/src/main/java/org/jude/managefront/controller/Hello.java
@@ -0,0 +1,12 @@
+package org.jude.managefront.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController      //=@Controller+@ResponseBody
+public class Hello {
+    @RequestMapping("/Hello")
+    public String testSpringBoot() {
+        return "恭喜您！测试成功，您可以开心的编写代码了";
+    }
+}
